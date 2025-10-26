8abc7a7ab46ee52744449defaebd55259c1b693d
chandra99-tech <chandra99iit@gmail.com>
2025-10-26 04:17:08 -0700

    Add files via upload

---
+++ day_06/day_06/src/exceptions/WrongOperatorException.java
@@ -0,0 +1,17 @@
+package exceptions;
+
+
+
+public class WrongOperatorException extends Exception {
+    public WrongOperatorException() {
+        super();
+    }
+    
+    public WrongOperatorException(String message) {
+        super(message);
+    }
+    
+    public WrongOperatorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
